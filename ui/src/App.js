cd1e5865b589c8824c783241c6d85f054dc6d6bd
Devyani Chowdhury <dchowdhury@microsoft.com>
2024-09-16 11:11:57 +0530

    WIP: UI react Project

---
+++ ui/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+// import D from './istockphoto-1125625274-612x612.jpg';
+import './App.css';
+import Navbar from './components/navbar'; // Import the Navbar component
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Navbar /> {/* Add the Navbar component here */}
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Stark Tech. Yes we work with Spiderman
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui/src/components/navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './css/navbar.css'; // Assuming you have a CSS file for styling
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <a href="/">Stark Tech</a>
+            </div>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a href="/home" className="nav-link">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/about" className="nav-link">About</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/questionnaire" className="nav-link">Questionnaire</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/calendar" className="nav-link">Calendar</a>
+                </li>
+                <li className='nav-item'>
+                    <a href="/contact" className="nav-link">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
