b349a65f02da2d017eb40cb257f6ccbb3986bd5c
Devyani Chowdhury <dchowdhury@microsoft.com>
2024-09-17 14:17:15 +0530

    WIP: Questionnaire CSS

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,35 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar';
 import Questionnaire from './components/questionnarie';
-import React, { useRef } from 'react';
-import Navbar from './components/navbar'; // Import the Navbar component
+import React from 'react';
 import MyCalendar from './components/calendar';
 
 function App() {
-  const questionnaireRef = useRef(null);
-
-    const scrollToQuestionnaire = () => {
-        if (questionnaireRef.current) {
-            questionnaireRef.current.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
   return (
     <div className="App">
-      <header className='App-header'>
-        <Navbar scrollToQuestionnaire={scrollToQuestionnaire} /> 
+      <Navbar /> 
+      <div className='Body'>
+        <div className='About'>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Stark Tech. Yes we work with Spiderman
-        </p>
-      </header>
-      <div className='About'></div>
-      <div className='Questionnaire'>
-        <section ref={questionnaireRef} id="questionnaire">
-                <Questionnaire />
-        </section>
+          <p>
+            Welcome to Stark Tech. Yes we work with Spiderman
+          </p>
+        </div>
+        <div className='Questionnaire'>
+          <section id="questionnaire">
+            <Questionnaire />
+          </section>
+        </div>
+        <div className='Calendar'>
+          <section id="calendar">
+            <MyCalendar/>
+          </section>
+        </div>
+        <div className='Contact'>
+          <section id="contact">
+          </section>
+        </div>
       </div>
-      <MyCalendar/>
     </div>
   );
 }
